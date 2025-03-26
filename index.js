273705ee441127bf98cebe2854c814cea893cd45
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-03-27 00:37:56 +0530

    ejs

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const path = require('path');
 
 
 const port = 3000;
-
+app.use(express.static( path.join(__dirname,"public")));
 app.set ('view engine', 'ejs');
-
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/",(req,res )=>{
